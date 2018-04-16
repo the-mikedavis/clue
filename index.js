a81be7abd6970f27c2121ad8796cc5c7b775fcea
Michael Davis <mcd164@miami.edu>
2018-04-15 23:18:49 -0400

    Switch from POST to GET API endpoint
    URL queries are more than enough to carry the weight of the query. The
    only complicated part will be the autocomplete which can be done by the
    page's Javascript.

--- index.js
+++ index.js
@@ -12,15 +12,10 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => res.render('index.html') );
 
-app.post('/results', function (req, res) {
-  console.log(arguments);
 // API endpoint here
+app.get('/results', function (req, res) {
+  console.log(req.query);
+  res.render('results.html', req.query);
 });
-
-/*
-app.get('/search', function(req, res) {
-  res.send(req.query);
-});
-*/
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
