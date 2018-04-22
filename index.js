8e595a118b1d8c7a668b25939b4a1ddf524f64e1
Michael Davis <mcd164@miami.edu>
2018-04-21 23:02:30 -0400

    Add the call to localhost for suggestions
    This is the first call to Solr for its internals. It's done by an
    asynchronous call that keeps the original `res` object in its scope.
    When it finishes asking Solr what's up, it sends the suggestions. If it
    has nothing to say, it just returns an empty string.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express'),
   nun = require('nunjucks'),
   path = require('path'),
   http = require('http'),
-  app = express();
+  app = express(),
+
+  CORE = 'lasflores'; //TODO temporary core name from Solr
 
 nun.configure('templates', {
   autoescape: true,
@@ -17,6 +19,19 @@ app.get('/', (req, res) => res.render('index.html') );
 app.get('/results', function (req, res) {
   console.log(req.query);
   res.render('results.html', req.query);
+});
+
+// API endpoint here too, for the suggestor module.
+app.get('/suggest', function (req, res) {
+  var query = req.query.q;
+  http.get('http://localhost:8983/solr/' + CORE +
+      '/suggest?suggest=true&suggest.dictionary=suggester&suggest.q=' +
+      escape(query),
+    function (internalRes) {
+      var data = '';
+      internalRes.on('data', chunk => data += chunk);
+      internalRes.on('end', () => res.send(data));
+    }).on('error', (err) => res.send(''));
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
