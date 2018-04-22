570f90982f423a43467217487f8cf023e3ae8d66
Michael Davis <mcd164@miami.edu>
2018-04-21 23:42:45 -0400

    Add the suggestions to browser-side code
    Just call the API

--- static/dist/js/search.js
+++ static/dist/js/search.js
@@ -1,1 +1,22 @@
 console.log("search.js loaded...");
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementById('searchBar').onchange = suggestionHandler;
+});
+
+function suggestionHandler (event) {
+  var text = event.target.value;
+  if (!text) return;
+  var httpReq;
+  if (window.XMLHttpRequest) httpReq = new XMLHttpRequest();
+  else if (window.ActiveXObject)
+    httpReq = new ActiveXObject('Microsoft.XMLHTTP');
+  else return;
+  httpReq.onreadystatechange = function () {
+    if (httpReq.readyState === XMLHttpRequest.DONE && httpReq.status === 200) {
+      console.log(httpReq.responseText);
+    }
+  };
+  httpReq.open('GET', '/suggest?q=' + escape(text));
+  httpReq.send();
+}
