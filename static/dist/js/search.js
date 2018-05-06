1402f737bfc88b0c867ca3fd436a6d0a41bce92c
Michael Davis <mcd164@miami.edu>
2018-05-06 18:42:36 -0400

    Use built-in HTML5 datalist for suggestions
    Screw IE.

--- static/dist/js/search.js
+++ static/dist/js/search.js
@@ -1,5 +1,3 @@
-console.log("search.js loaded...");
-
 function suggest (event) {
   var text = event.target.value;
   if (!text) return;
@@ -10,9 +8,32 @@ function suggest (event) {
   else return;
   httpReq.onreadystatechange = function () {
     if (httpReq.readyState === XMLHttpRequest.DONE && httpReq.status === 200) {
-      console.log(httpReq.responseText);
+      try {
+        var response = JSON.parse(httpReq.responseText);
+      } catch (e) {
+        return;
+      }
+      clearSuggestions();
+      // what a mouthful
+      makeSuggestions(response.suggest.suggester[text].suggestions);
     }
   };
   httpReq.open('GET', '/suggest?q=' + escape(text));
   httpReq.send();
+}
+
+// see https://stackoverflow.com/a/3955238/7232773
+function clearSuggestions() {
+  var datalist = document.getElementById('suggestions');
+  while (datalist.lastChild)
+    datalist.removeChild(datalist.lastChild);
+}
+
+function makeSuggestions(sugs) {
+  var datalist = document.getElementById('suggestions');
+  for (var i = 0; i < sugs.length; i++) {
+    var option = document.createElement('option');
+    option.value = sugs[i].term;
+    datalist.appendChild(option);
+  }
 }
