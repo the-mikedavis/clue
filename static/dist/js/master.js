9ef5e92298d8f52a15452b268dccd778698c972e
Michael Davis <mcd164@miami.edu>
2018-05-07 09:49:28 -0400

    Implement select for download

--- static/dist/js/master.js
+++ static/dist/js/master.js
@@ -2,6 +2,10 @@ window.addEventListener('load', function () {
   var tabbtns = document.querySelectorAll('ul.tab-buttons > li');
   for (var i = 0; i < tabbtns.length; i++)
     tabbtns[i].onclick = clickTab;
+
+  var docs = document.getElementsByClassName('document');
+  for (var i = 0; i < docs.length; i++)
+    docs[i].onclick = addToSelection;
 });
 
 function clickTab(evt) {
@@ -17,4 +21,20 @@ function hideAll() {
   var els = document.querySelectorAll('.active');
   for (var i = 0; i < els.length; i++)
     els[i].classList.remove('active');
+}
+
+function addToSelection (evt) {
+  // bubble
+  var el = evt.target;
+  while (el != null) {
+    if (el.classList && el.classList.contains('document'))
+      break;
+    el = el.parentElement;
+  }
+  if (el == null)
+    return;
+  else if (el.classList.contains('selected'))
+    el.classList.remove('selected');
+  else
+    el.classList.add('selected');
 }
